ce1411e4d08dbff7e0751b749681a2cdb23e0ec2
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-07-09 23:26:42 +0300

    build

---
+++ out/_next/static/chunks/app/d/page-b08de219fde6aec7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[311,931],{1598:function(n,e,u){Promise.resolve().then(u.t.bind(u,4712,23))}},function(n){n.O(0,[712,253,769,744],function(){return n(n.s=1598)}),_N_E=n.O()}]);
